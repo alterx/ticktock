ff7171effff294cb81d627609ed695ab1581066a
Carlos Vega Rodriguez <carlosvegarodriguez@Carloss-MacBook-Pro.local>
2015-05-31 21:29:24 -0600

    first page commit

--- filters/time-compact.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('tickTock').filter('timeCompact', function () {
-        return function(input) {
-            return moment(input.toISOString()).format('hh:mm a');
-        };
-    });
-})();
--- filters/time.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('tickTock').filter('time', function () {
-        return function(input) {
-            return input.format('hh:mm:ss');
-        };
-    });
-})();
